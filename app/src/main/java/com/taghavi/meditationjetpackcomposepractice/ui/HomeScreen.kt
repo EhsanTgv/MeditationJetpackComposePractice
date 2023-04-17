f6d9e595dcf887e1d2960503d7ab8235087ba486
EhsanTgv <EhsanTgv@gmail.com>
2023-04-17 17:06:57 +0300

    Implement HomeScreen

---
+++ app/src/main/java/com/taghavi/meditationjetpackcomposepractice/ui/HomeScreen.kt
@@ -0,0 +1,22 @@
+package com.taghavi.meditationjetpackcomposepractice.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.taghavi.meditationjetpackcomposepractice.ui.theme.DeepBlue
+
+@Composable
+fun HomeScreen() {
+    Box(
+        modifier = Modifier
+            .background(DeepBlue)
+            .fillMaxSize()
+    ){
+        Column {
+
+        }
+    }
+}
