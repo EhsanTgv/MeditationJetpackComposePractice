004937b12aaf986f6ddc292d8a844400d78c1896
EhsanTgv <EhsanTgv@gmail.com>
2023-04-26 17:07:31 +0300

    Implement Greeting Section

--- app/src/main/java/com/taghavi/meditationjetpackcomposepractice/MainActivity.kt
+++ app/src/main/java/com/taghavi/meditationjetpackcomposepractice/MainActivity.kt
@@ -3,6 +3,7 @@ package com.taghavi.meditationjetpackcomposepractice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.taghavi.meditationjetpackcomposepractice.ui.HomeScreen
 import com.taghavi.meditationjetpackcomposepractice.ui.theme.MeditationUIYouTubeTheme
 
 class MainActivity : ComponentActivity() {
@@ -10,7 +11,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MeditationUIYouTubeTheme {
-
+                HomeScreen()
             }
         }
     }
